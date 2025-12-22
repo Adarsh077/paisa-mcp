4613d77a60e09487f6d533b3d691fc9fd5e24b3c
Adarsh Senghani <adarshco077@gmail.com>
2025-12-22 22:57:16 +0530

    feat: implemented sst for deployment

--- sst.config.ts
+++ sst.config.ts
@@ -9,5 +9,26 @@ export default $config({
       home: "aws",
     };
   },
-  async run() {},
+  async run() {
+    const PAISA_API_BASE_URL = new sst.Secret("PAISA_API_BASE_URL");
+
+    const vpc = new sst.aws.Vpc("PaisaMcpVpc", {
+      az: ["ap-south-1a", "ap-south-1b", "ap-south-1c"],
+    });
+
+    const cluster = new sst.aws.Cluster("PaisaMcpCluster", { vpc });
+
+    const service = new sst.aws.Service("PaisaMcpService", {
+      cluster,
+      dev: false,
+      serviceRegistry: {
+        port: 8000,
+      },
+      environment: { PAISA_API_BASE_URL: PAISA_API_BASE_URL.value },
+    });
+
+    const api = new sst.aws.ApiGatewayV2("PaisaMcpApi", { vpc });
+
+    api.routePrivate("$default", service.nodes.cloudmapService.arn);
+  },
 });
